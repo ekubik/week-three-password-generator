7713088558e063b7ce019953b6c7527d113f532e
Ewa Kubik <ewa.kubik@hotmail.com>
2021-12-06 23:53:00 +1100

    updated script.js. prompt user to input information.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,68 @@
+//declare variables
+var passwordLength;
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseAbc = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var upperCaseAbc = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var specialChar = ["!", "#", "$", "%", "&", "*", "/", "?", "^", "_"];
+var includeNumbers;
+var includeLowerCaseAbc;
+var includeUpperCaseAbc;
+var includeSpecialChar;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,10 +70,25 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = prompt(
+    "Please enter desired password length. This should be between 8 and 128 characters"
+  );
+  if (passwordLength < 7 || passwordLength > 128) {
+    alert("Please enter a valid password length");
+    prompt("Enter a password length between 8 and 128 characters");
+  } else {
+    var includeNumbers = confirm(
+      "Would you like to include numerical characters?"
+    );
+    var includeLowerCaseAbc = confirm(
+      "Would you like to include lower case letters?"
+    );
+  }
+}
